19e23c0ce1e0885360cc97c74f66b983bda84f04
Yurkines <6eJlomop@inbox.ru>
2023-03-17 00:04:09 +0300

    First stage

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.sql.SQLOutput;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int year = 3824;
+        int clientOS = 0;
+        int clientDeviceYear = LocalDate.now().getYear();
+        leapYearVerification(year);
+        osUpdate(clientOS, clientDeviceYear);
+    }
+
+    public static void leapYearVerification(int leapYear) {
+        if (leapYear % 4 == 0 && leapYear % 100 != 0) {
+            System.out.println("Год " + leapYear + " является високосным");
+        } else if (leapYear % 400 == 0) {
+            System.out.println("Год " + leapYear + " является високосным");
+        } else {
+            System.out.println("Год " + leapYear + " не является високосным");
+        }
+    }
+
+    public static void osUpdate(int clientOS, int clientDeviceYear) {
+        int iOS = 0;
+        int androidOS = 1;
+        if (clientOS == iOS && clientDeviceYear >= 2015 ) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == iOS && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        if (clientOS == androidOS && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == androidOS && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
     }
 }
